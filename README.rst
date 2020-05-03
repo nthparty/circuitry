93e1292a21ffdad8840965066c84f81b720c41a3
Andrei Lapets <a@lapets.io>
2020-05-02 21:10:17 -0400

    Refactor/add synthesis state methods and operation methods.

--- README.rst
+++ README.rst
@@ -10,7 +10,7 @@ Embedded domain-specific combinator library for assembling abstract definitions 
 
 Purpose
 -------
-This embedded domain-specific language (DSL) makes it possible to write an algorithm in Python that operates over bit vectors, and then to interpret that algorithm implementation as a circuit definition.
+This embedded domain-specific language (DSL) makes it possible to write an algorithm in Python that operates over bit vectors, and then to interpret that algorithm implementation as a circuit definition in order to synthesize a logic circuit.
 
 Package Installation and Usage
 ------------------------------
--- circuitry/circuitry.py
+++ circuitry/circuitry.py
@@ -20,77 +20,163 @@ class bit():
     circuit built up out of those operators.
     """
 
+    _circuit = None
+    _hook_operation = None
+
     @staticmethod
     def circuit(circuit_ = None):
         if circuit_ is not None:
             bit._circuit = circuit_
         else:
-            bit._circuit.prune_and_stable_topological_sort(lambda b: type(b) is output)
+            bit._circuit.prune_and_topological_sort_stable()
             return bit._circuit
+
+    @staticmethod
+    def hook_operation(hook = None):
+        bit._hook_operation = hook
+
+    @staticmethod
+    def operation(o, *args):
+        # Ensure second argument is a `bit`.
+        args = list(args)
+        if len(args) == 2:
+            args[1] = constant(args[1]) if type(args[1]) is int else args[1]
+
+        # Compute the value of the result of the operation on the arguments.
+        v = o(*[a.value for a in args])
+
+        # Return output from hook if it exists and if
+        # it returns an output.
+        if bit._hook_operation is not None:
+            r = bit._hook_operation(o, v, *args)
+            if r is not None:
+                return r
+
+        return bit.constructor(*args)(v, bit.gate(o, [a.gate for a in args]))
+
+    @staticmethod
+    def constructor(b1, b2 = None):
+        return bit
+
+        # The inference code below is not currently in use.
+        if type(b1) is input_one and type(b2) is input_one:
+            return input_one
+        elif type(b1) is input_two and type(b2) is input_two:
+            return input_two
+        elif type(b1) in [input_one, input_two] and b2 is None:
+            return type(b1)
+        else:
+            return bit
+
+    @staticmethod
+    def gate(operation, igs):
+        g = bit._circuit.gate(operation, igs)
+        for ig in igs:
+            ig.output(g)
+        return g
 
     def __init__(self, value, gate_ = None):
         self.value = value
-        self.gate = gate(attributes = {'bit': self}) if gate_ is None else gate_
+        self.gate = bit._circuit.gate() if gate_ is None else gate_
 
-    def __invert__(self: bit) -> bit:
-        return bit(1 - self.value)
+    def __int__(self):
+        return self.value
 
-    def __pos__(self: bit) -> bit:
-        return bit(self.value)
+    def not_(self):
+        return bit.operation(op.not_, self)
 
-    def __and__(self: bit, other: bit) -> bit:
-        return bit(self.value & other.value)
+    def __invert__(self):
+        return bit.operation(op.not_, self)
 
-    def __rand__(self: bit, other) -> bit:
+    def and_(self, other):
+        return bit.operation(op.and_, self, other)
+
+    def __and__(self, other):
+        return bit.operation(op.and_, self, other)
+
+    def __rand__(self, other):
         return self & (constant(other) if type(other) is int else other)
 
-    def __gt__(self: bit, other: bit) -> bit: # NIMP operation.
-        return bit(1 if self.value > other.value else 0)
+    def nimp(self, other):
+        return bit.operation(op.nimp_, self, other)
 
-    def __lshift__(self: bit, other: bit) -> bit: # FST operation.
-        return bit(self.value)
+    def nimp_(self, other):
+        return bit.operation(op.nimp_, self, other)
 
-    def __lt__(self: bit, other: bit) -> bit: # NIF operation.
-        return bit(1 if self.value < other.value else 0)
+    def __gt__(self, other):
+        return self.nimp(other)
 
-    def __rshift__(self: bit, other: bit) -> bit: # SND operation.
-        return bit(other.value)
+    def nif(self, other):
+        return bit.operation(op.nif_, self, other)
 
-    def __xor__(self: bit, other: bit) -> bit:
-        return bit(self.value ^ other.value)
+    def nif_(self, other):
+        return bit.operation(op.nif_, self, other)
 
-    def __rxor__(self: bit, other) -> bit:
+    def __lt__(self, other):
+        return self.nif(other)
+
+    def xor(self, other):
+        return bit.operation(op.xor_, self, other)
+
+    def xor_(self, other):
+        return bit.operation(op.xor_, self, other)
+
+    def __xor__(self, other):
+        return bit.operation(op.xor_, self, other)
+
+    def __rxor__(self, other):
         return self ^ (constant(other) if type(other) is int else other)
 
-    def __or__(self: bit, other: bit) -> bit:
-        return bit(self.value | other.value)
+    def or_(self, other):
+        return bit.operation(op.or_, self, other)
 
-    def __ror__(self: bit, other) -> bit:
+    def __or__(self, other):
+        return bit.operation(op.or_, self, other)
+
+    def __ror__(self, other):
         return self | (constant(other) if type(other) is int else other)
 
-    def __mod__(self: bit, other: bit) -> bit: # NOR operation.
-        return bit(0 if self.value | other.value else 1)
+    def nor(self, other):
+        return bit.operation(op.nor_, self, other)
 
-    def __eq__(self: bit, other: bit) -> bit: # XNOR operation.
-        return bit(1 if self.value == other.value else 0)
+    def nor_(self, other):
+        return bit.operation(op.nor_, self, other)
 
-    def __floordiv__(self: bit, other: bit) -> bit: # NSND operation.
-        return bit(1 - other.value)
+    def __mod__(self, other):
+        return bit.operation(op.nor_, self, other)
 
-    def __ge__(self: bit, other: bit) -> bit: # IF operation.
-        return bit(1 if self.value >= other.value else 0)
+    def xnor(self, other):
+        return bit.operation(op.xnor_, self, other)
 
-    def __truediv__(self: bit, other: bit) -> bit: # NFST operation.
-        return bit(1 - self.value)
+    def xnor_(self, other):
+        return bit.operation(op.xnor_, self, other)
 
-    def __le__(self: bit, other: bit) -> bit: # IMP operation.
-        return bit(1 if self.value <= other.value else 0)
+    def __eq__(self, other):
+        return bit.operation(op.xnor_, self, other)
 
-    def __matmul__(self: bit, other: bit) -> bit: # NAND operation.
-        return bit(0 if self.value & other.value else 1)
+    def if_(self, other):
+        return bit.operation(op.if_, self, other)
 
-    def __int__(self: bit) -> int:
-        return self.value
+    def __ge__(self, other):
+        return bit.operation(op.if_, self, other)
+
+    def imp(self, other):
+        return bit.operation(op.imp_, self, other)
+
+    def imp_(self, other):
+        return bit.operation(op.imp_, self, other)
+
+    def __le__(self, other):
+        return bit.operation(op.imp_, self, other)
+
+    def nand(self, other):
+        return bit.operation(op.nand_, self, other)
+
+    def nand_(self, other):
+        return bit.operation(op.nand_, self, other)
+
+    def __matmul__(self, other):
+        return bit.operation(op.nand_, self, other)
 
 class constant(bit):
     """Bit that is designated as a constant input."""
@@ -98,7 +184,10 @@ class constant(bit):
 
 class input(bit):
     """Bit that is designated as a variable input."""
-    pass
+
+    def __init__(self: bit, value: int):
+        self.value = value
+        self.gate = bit._circuit.gate(op.id_, is_input = True)
 
 class input_one(input):
     """Bit that is designated as a variable input from one source."""
@@ -113,9 +202,7 @@ class output(bit):
 
     def __init__(self: bit, b: bit):
         self.value = b.value
-        self.gate = b.gate
-        bit._circuit.final(self)
-        self.gate.bit = self
+        self.gate = bit._circuit.gate(op.id_, [b.gate], is_output = True)
 
 class bits(list):
     """
@@ -153,12 +240,23 @@ class bits(list):
     def __or__(self: bits, other: bits):
         return bits([x | y for (x, y) in zip(self, other)])
 
-    def __rshift__(self: bits, other: int) -> bits:
+    def nif(self: bits, other: bits) -> bits:
+        return bits([x.nif(y) for (x, y) in zip(self, other)])
+
+    def nimp(self: bits, other: bits) -> bits:
+        return bits([x.nimp(y) for (x, y) in zip(self, other)])
+
+    def __rshift__(self: bits, other):
         return bits([constant(0)]*other) @ bits(self[0:len(self)-other])
+
+    def __lshift__(self: bits, other):
+        return bits(self[other:]) @ bits([constant(0) for _ in range(other)])
 
     def __truediv__(self: bits, other) -> Sequence[bits]:
         if type(other) is list and len(other) > 0 and type(other[0]) is int:
-            return self / (len(self)//other[0]) # Parts of length `other`.
+            return map(bits, parts(self, length=other)) # Number of parts is `other`.
+        elif type(other) is set and len(other) == 1 and type(list(other)[0]) is int:
+            return self / (len(self)//list(other)[0]) # Parts of length `other`.
         else:
             return map(bits, parts(self, other)) # Number of parts is `other`.
 
@@ -172,6 +270,15 @@ class bits(list):
 
     def __int__(self: bits) -> int:
         return sum(int(b)*(2**i) for (i, b) in zip(range(len(self)), reversed(self)))
+
+def constants(l):
+    return bits(map(constant, l))
+
+def inputs(l):
+    return bits(map(input, l))
+
+def outputs(l):
+    return bits(map(output, l))
 
 if __name__ == "__main__":
     doctest.testmod()
