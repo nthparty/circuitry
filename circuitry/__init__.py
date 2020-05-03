75a768081b249df243836d314375c5cc95f1248e
Andrei Lapets <a@lapets.io>
2020-05-02 21:15:24 -0400

    Give users access to new symbols.

--- circuitry/__init__.py
+++ circuitry/__init__.py
@@ -1,6 +1,4 @@
 # Allow users to access symbols directly.
 from circuitry.circuitry import\
-    bit,\
-    constant,\
-    input, input_one, input_two, output,\
-    bits
+    bit, constant, input, input_one, input_two, output,\
+    bits, constants, inputs, outputs
