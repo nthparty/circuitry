035120e87a07cb436a809d88866415ea3f5d735d
Andrei Lapets <a@lapets.io>
2023-01-29 22:53:43 -0500

    Adjust docs config to automatically find/include testing script.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src')) # Prioritize local module copy.
+sys.path.insert(0, os.path.abspath('..')) # Include source and testing script.
 
 
 # -- Project information -----------------------------------------------------
